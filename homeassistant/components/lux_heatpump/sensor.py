2edfb606e67aff9c798453e48a143721e6ca84ee
Bernd Jungblut <bernd.jungblut@gmail.com>
2024-09-20 09:16:26 +0000

    init heatpump skeleton

---
+++ homeassistant/components/lux_heatpump/sensor.py
@@ -0,0 +1,73 @@
+"""Platform for sensor integration."""
+
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor1()])
+    add_entities([ExampleSensor2()])
+    add_entities([ExampleSensor3()])
+
+
+class ExampleSensor1(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Outdoor temperature"
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 23
+
+
+class ExampleSensor2(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "heating circuit flow temperature"
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant .
+        """
+        self._attr_native_value = 23
+
+
+class ExampleSensor3(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "heating circuit return flow temperature (actual)"
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 23
