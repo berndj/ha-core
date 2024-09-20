3a5b691e5b3472cdea20dc492c17ebb910be6d1f
Bernd Jungblut <bernd.jungblut@gmail.com>
2024-09-20 18:38:08 +0000

    add socket recovery

--- homeassistant/components/lux_heatpump/heatpump_engine.py
+++ homeassistant/components/lux_heatpump/heatpump_engine.py
@@ -22,9 +22,33 @@ class heatpump_engine:
         self.polls = 0
         self.polls_skipped = 0
         self.epoch_time = int(time.time())
+        self.host = None
+        self.port = None
+
+    def is_socket_closed(self, sock: socket.socket) -> bool:
+        """Check socket closed state."""
+        try:
+            # this will try to read bytes without blocking and also without removing them from buffer (peek only)
+            data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
+            if len(data) == 0:
+                return True
+        except BlockingIOError:
+            return False  # socket is open and reading from it would block
+        except ConnectionResetError:
+            return True  # socket was closed for some other reason
+        return False
+
+    def check_socket(self):
+        """Check and repair socket."""
+
+        if self.is_socket_closed(self.sock):
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.host, self.port))
 
     def connect(self, host, port):
         """Connect to ser2net socket."""
+        self.host = host
+        self.port = port
         try:
             self.sock.connect((host, port))
         except ConnectionError:
@@ -34,12 +58,13 @@ class heatpump_engine:
     def readlines(self):
         """Read answer from ser2net/heatpump."""
         self.sock.settimeout(0.1)
-
         while True:
             try:
                 data = self.sock.recv(1024)
             except TimeoutError:
-                #                print("======\nEOF\n=")
+                break
+            except ConnectionAbortedError:
+                self.reinit()
                 break
             lines = data.split(b"\r\n")
             for line in lines:
@@ -48,7 +73,10 @@ class heatpump_engine:
     def trigger_stats(self):
         """Trigger response from heatpump."""
         buf = "1800\n\r"
-        self.sock.send(buf.encode(encoding="utf-8"))
+        try:
+            self.sock.send(buf.encode(encoding="utf-8"))
+        except ConnectionAbortedError:
+            self.reinit()
 
     def extract_temp(self, line):
         """Extract temperature values from response."""
@@ -82,6 +110,7 @@ class heatpump_engine:
 
         new_time = int(time.time())
         if new_time - self.epoch_time > 10 or self.polls == 0:
+            self.check_socket()
             self.trigger_stats()
             self.readlines()
             self.epoch_time = new_time
