db568b9f22c2ea22a8233d52817842eaca1ad1f9
Bernd Jungblut <bernd.jungblut@gmail.com>
2024-10-03 16:58:00 +0000

    add static connection state

--- homeassistant/components/lux_heatpump/__init__.py
+++ homeassistant/components/lux_heatpump/__init__.py
@@ -1,1 +1,12 @@
-"""The example sensor integration."""
+"""Luxtronik v1 heatpump sensor integration."""
+
+DOMAIN = "lux_heatpump"
+
+
+async def async_setup(hass, config):
+    """Integration setup."""
+
+    hass.states.async_set("lux_heatpump.connection", "up")
+
+    # Return boolean to indicate that initialization was successful.
+    return True
